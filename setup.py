083e1ec384ff341b44ce496f4a0c49686dc0c100
Oleg Yunin <oayunin@gmail.com>
2017-02-12 18:20:18 +0300

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
       name='academic_nausea',
-      version='0.0.1',
+      version='0.1.0',
       author='Oleg Yunin',
       author_email='oayunin@gmail.com',
       url='https://github.com/walkingpendulum/academic_nausea',
